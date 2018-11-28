f994ab13b37e92328ac78320e9f436658bfd2432
Admin <admin@AppCraft-MacBook-Pro.local>
2018-11-28 19:07:09 +0300

    change test img, fix upside down image orientation

--- PhotoEditor/Sources/Services/PhotoEditorService.swift
+++ PhotoEditor/Sources/Services/PhotoEditorService.swift
@@ -6,56 +6,37 @@ import CoreImage
 import ImageIO
 import UIKit
 
+typealias EditCallback = (UIImage?) -> Void
+
 protocol PhotoEditorServiceType {
-    func resize(_ image: UIImage, to size: CGSize, callback: (UIImage?) -> Void)
+    func resize(_ image: UIImage, to size: CGSize, callback: @escaping EditCallback)
     func cropeZone(_ zone: CGRect, of image: UIImage) -> UIImage?
     func rotateImage(_ image: UIImage, byDegrees degrees: CGFloat, clockwise: Bool) -> UIImage?
-    func filterImage(_ image: UIImage, withFilterNamed name: String) -> UIImage?
+    func applyFilterNamed(_ name: String, to image: UIImage, withOptions options: [String: Any]) -> UIImage?
     func changeColor(of image: UIImage, withValue value: CGFloat) -> UIImage?
     func changedBrightness(of image: UIImage, withValue value: CGFloat) -> UIImage?
 }
 
-final class PhotoEditorService {
+final class PhotoEditorService: PhotoEditorServiceType {
     init(options: [CIContextOption: Any]? = [.useSoftwareRenderer: false]) {
         self.context = CIContext(options: options)
-    }
-
-    func resize(_ image: UIImage, to size: CGSize, callback: @escaping (UIImage?) -> Void) {
-        guard let image = image.cgImage else {
-            callback(nil)
-            return
-        }
-
-        DispatchQueue.global().async {
-            let scale = min(size.width / CGFloat(image.size.width),
-                            size.height / CGFloat(image.size.height))
-
-            let size = CGSize(width: CGFloat(image.size.width) * scale,
-                              height: CGFloat(image.size.height) * scale)
-
-            let renderer = UIGraphicsImageRenderer(size: size)
-            let img = renderer.image {
-                $0.cgContext.saveGState()
-                $0.cgContext.draw(image, in: CGRect(origin: .zero, size: size))
-                $0.cgContext.restoreGState()
-            }
-
-            DispatchQueue.main.async {
-                print(img.size)
-                callback(img)
-            }
-        }
     }
 
     func cropeZone(_ zone: CGRect, of image: UIImage) -> UIImage? {
         return image.cropedZone(zone)
     }
 
-    func rotateImage(_ image: UIImage, byDegrees degrees: CGFloat, clockwise: Bool) -> UIImage? {
-        return nil
+    func asyncApplyFilterNamed(_ name: String, to image: UIImage, with callback: @escaping EditCallback) {
+        DispatchQueue.global().async { [weak self] in
+            let img = self?.applyFilterNamed(name, to: image)
+
+            DispatchQueue.main.async {
+                callback(img)
+            }
+        }
     }
 
-    func applyFilter(named name: String, to image: UIImage, withOptions options: [String: Any] = [:]) -> UIImage? {
+    func applyFilterNamed(_ name: String, to image: UIImage, withOptions options: [String: Any] = [:]) -> UIImage? {
         guard let filter = CIFilter(name: name), let ciImage = CIImage(image: image) else {
             return nil
         }
@@ -70,22 +51,50 @@ final class PhotoEditorService {
         return UIImage(cgImage: cgResult)
     }
 
-    func changeColor(of image: UIImage, withValue value: CGFloat) -> UIImage? {
-        return nil
-    }
+    func resize(_ image: UIImage, to size: CGSize, callback: @escaping EditCallback) {
+        guard let image = image.cgImage else {
+            callback(nil)
+            return
+        }
 
-    func changedBrightness(of image: UIImage, withValue value: CGFloat) -> UIImage? {
-        return nil
-    }
+        DispatchQueue.global().async {
+            let scale = min(size.width / CGFloat(image.size.width),
+                            size.height / CGFloat(image.size.height))
 
-    func asyncApplyFilterNamed(_ name: String, to image: UIImage, with callback: @escaping (UIImage?) -> Void) {
-        DispatchQueue.global().async { [weak self] in
-            let img = self?.applyFilter(named: name, to: image)
+            let size = CGSize(width: CGFloat(image.size.width) * scale,
+                              height: CGFloat(image.size.height) * scale)
+
+            let renderer = UIGraphicsImageRenderer(size: size)
+            let img = renderer.image {
+//                $0.cgContext.saveGState()
+                $0.cgContext.translateBy(x: 0, y: size.height)
+                $0.cgContext.scaleBy(x: 1.0, y: -1.0)
+                $0.cgContext.draw(image, in: CGRect(origin: .zero, size: size))
+//                $0.cgContext.restoreGState()
+            }
 
             DispatchQueue.main.async {
                 callback(img)
             }
         }
+    }
+
+    // FIXME: Add logic
+
+    func changeColor(of image: UIImage, withValue value: CGFloat) -> UIImage? {
+        return nil
+    }
+
+    // FIXME: Add logic
+
+    func changedBrightness(of image: UIImage, withValue value: CGFloat) -> UIImage? {
+        return nil
+    }
+
+    // FIXME: Add logic
+
+    func rotateImage(_ image: UIImage, byDegrees degrees: CGFloat, clockwise: Bool) -> UIImage? {
+        return nil
     }
 
     private let context: CIContext
