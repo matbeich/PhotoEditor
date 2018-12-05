a72c2b8b4d7aee5e127aed86941feeedb730961a
Admin <admin@AppCraft-MacBook-Pro.local>
2018-12-05 15:12:51 +0300

    move app filters and config to editor kit

---
+++ PhotoEditorKit/Common/Config.swift
@@ -0,0 +1,11 @@
+//
+// Copyright © 2018 Dimasno1. All rights reserved. Product:PhotoEditor
+//
+
+import CoreGraphics
+
+public enum Config {
+    public static let cropViewMinDimension: CGFloat = 100.0
+    public static let toolBarHeight: CGFloat = 59.0
+}
+
