0fe002ec330047582b0d0162ca8a4ba5449bfbc5
Admin <admin@AppCraft-MacBook-Pro.local>
2019-01-18 16:27:26 +0300

    add face detector to app context

--- PhotoEditorKit/Common/AppContext.swift
+++ PhotoEditorKit/Common/AppContext.swift
@@ -5,16 +5,21 @@
 import UIKit
 
 public class AppContext {
-    public init(photoEditService: PhotoEditorService = PhotoEditorService(),
-                stateStore: StateStore<AppState> = StateStore(State<AppState>(.initial)),
-                calculator: GeometryCalculator = GeometryCalculator()) {
+    public init(
+        photoEditService: PhotoEditorService = PhotoEditorService(),
+        stateStore: StateStore<AppState> = StateStore(State<AppState>(.initial)),
+        calculator: GeometryCalculator = GeometryCalculator(),
+        faceDetector: FaceDetector = FaceDetector()
+        ) {
         self.photoEditService = photoEditService
         self.stateStore = stateStore
         self.calculator = calculator
+        self.faceDetector = faceDetector
     }
 
     public let photoEditService: PhotoEditorService
     public let stateStore: StateStore<AppState>
     public let calculator: GeometryCalculator
+    public let faceDetector: FaceDetector
 }
 
